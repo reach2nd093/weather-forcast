c0f906e9f70d6f54127653096e7c53ef657ca2e4
reach2nd093 <reach2nd093@gmail.com>
2023-10-19 18:28:15 +0900

    Basic setup for the components + Added fonts

---
+++ src/components/CurrentForcast/CurrentForcast.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './CurrentForcast.css';
+
+function CurrentForcast() {
+  return <div className="currentForcast">This is the current forcast component</div>;
+}
+
+export default CurrentForcast;
---
+++ src/components/DaySpecificForcast/DaySpecificForcast.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './DaySpecificForcast.css';
+
+function DaySpecificForcast() {
+  return <div className="DaySpecificForcast">This is the Day specific forcast</div>;
+}
+
+export default DaySpecificForcast;
